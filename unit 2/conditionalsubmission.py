7826a82fb5854e056d179ec528f0712fc105cf83
Keitam24 <keitam329@boyslatin.net>
2025-09-25 14:24:51 +0000

    todays commit 9.25.25

---
+++ unit 2/conditionalsubmission.py
@@ -0,0 +1,41 @@
+def permittest(age): 
+
+   
+    if age >= 16:
+        print(' You are old enough to get your drivers permit, congratulations.')
+    else:
+        print('Sorry, you are not able to get your drivers permit.')
+ 
+permittest(16)
+
+
+
+# 2
+
+def problemtwo(integer):
+    if integer >= 0:
+        print('This is positive.')
+    else:
+       print('This is negative.')
+
+
+problemtwo(-3)       
+
+
+
+def problemthree(yourgrade):
+    if yourgrade >= 90:
+        print(' You have an A.')
+    elif yourgrade <= 90 and yourgrade >= 80:
+        print('You have a B.')
+    elif yourgrade <= 80 and yourgrade >= 70:
+        print('You have a C.')
+    elif yourgrade <= 70 and yourgrade >= 60:
+        print('Your grade is a D.') 
+    else:
+        print ('You have a F')
+
+
+
+
+problemthree(80)
--- unit 2/conditions.py
+++ unit 2/conditions.py
@@ -5,24 +5,24 @@
 # if keyword - controls the condition that we want to satisfy.
 
 # else - Our default outcome. the things that happens when our conditions are not satisfied.
+def sunnyday():
+    weather = input('what is the weather like today? ')
+    if weather == 'sunny':
+        print(" it is beautiful outside. Bring sunglasses. So you don't have to see Joel.")
+    elif weather == 'rainy': 
+        print(' Bring an umbrella and hit joel with it')
+    elif weather == 'windy':
+        print(' Bring a jacket and kick Joel')
+    else: 
+        print("I can't tell you the weather, but have a good day unless your name is Joel.")
 
-weather = input('what is the weather like today? ')
-if weather == 'sunny':
-    print(" it is beautiful outside. Bring sunglasses. So you don't have to see Joel.")
-elif weather == 'rainy': 
-    print(' Bring an umbrella and hit joel with it')
-elif weather == 'windy':
-    print(' Bring a jacket and kick Joel')
-else: 
-    print("I can't tell you the weather, but have a good day unless your name is Joel.")
+def yourpassword():
+    password = input('Please enter your password.')
+    if password == 'skibiditoilet':
+        print(' Welcome to your account.')
 
-
-password = input('Please enter your password.')
-if password == 'skibiditoilet':
-    print(' Welcome to your account.')
-
-else:
-    print("Your password or email was incorrect, please try again.")
+    else:
+        print("Your password or email was incorrect, please try again.")
 
  
 
@@ -41,25 +41,27 @@ def iceCreamshop(vanilla):
          print('we dont have that ice cream get out')       
   
 
+def football():
+    down = input('What down is it')
+    yards = input('How many yards do you need to get another first down? ')
 
-down = input('What down is it')
-yards = input('How many yards do you need to get another first down? ')
-
-if down == 1 and yards <= 5:
-    print('rush')
-elif down == 2 and yards <= 5:
-    print('rush')
-elif down == 3 and yards <= 5:
-    print('pass')
-
-else:
-   print('punt')   
-
+    if down == 1 and yards <= 5:
+        print('rush')
+    elif down == 2 and yards <= 5:
+        print('rush')
+    elif down == 3 and yards <= 5:
+        print('pass')
+    else:
+        print('punt')   
 
 
 
-age = input('Are you old enough to drive? ')
-if age >= '16':
-    print(' You are old enough to drive, congratulations.')
-else:
-    print('Sorry, you are not able to drive a car.')
+def permittest(age): 
+
+   
+    if age >= 16:
+        print(' You are old enough to get your drivers permit, congratulations.')
+    else:
+        print('Sorry, you are not able to get your drivers permit.')
+ 
+permittest(16)
