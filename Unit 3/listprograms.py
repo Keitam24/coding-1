5f714c7adee9be798514cf1b892942f8ff2a9595
Keitam24 <keitam329@boyslatin.net>
2025-10-14 14:21:58 +0000

    listprograms lalala 10/14/25

---
+++ Unit 3/listprograms.py
@@ -0,0 +1,12 @@
+thanksgiving = ['Mac and Cheese', 'Turkey', 'Mashed Potatoes', 'Collard Greens']
+
+print(thanksgiving[0])
+print(thanksgiving[3])
+thanksgiving[3] = 'Stuffing'
+
+print(thanksgiving) 
+
+thanksgiving.pop(1)
+thanksgiving.pop(2)
+print(thanksgiving)
+
