b42bca869a50f6ec21b5a198e17191159dd7a311
Keitam24 <keitam329@boyslatin.net>
2025-10-15 14:11:59 +0000

    listprogram questions finished

--- Unit 3/listprograms.py
+++ Unit 3/listprograms.py
@@ -10,3 +10,29 @@ thanksgiving.pop(1)
 thanksgiving.pop(2)
 print(thanksgiving)
 
+
+def todaysmeal():
+    print ('Hello, what time of day is it today?')
+    print(' 1. morning')
+    print ('2. afternoon')
+    print ('3. evening')
+    select= int(input('Please choose the time: '))
+    if select == 1:
+        morningmenu = ['Panini', 'Bagel', 'Bacon egg and cheese']
+        print(morningmenu)
+        print('It is morning, please choose an item')
+    elif select == 2:     
+           lunchmenu = ['Hoagie', 'Pizza', 'Wing combo']
+           print(lunchmenu)
+           print('It is afternoon, please choose an item for lunch')
+
+    elif select == 3:
+         dinnermenu = ['Soup',  'Steak and fries', 'Chicken Alfredo']
+         print(dinnermenu)
+         print('It is evening, please choose an item for dinner')
+
+    else:
+         print('Sorry, we dont recognize your input. Please input it again.')      
+
+
+todaysmeal()    
